44877158fccedfe399c64fa37765d3ccea645409
Frederico G. Costa <fredgcosta@gmail.com>
2020-05-06 15:33:43 -0300

    fix: correções diversas para corrigir métricas

--- src/main/java/com/example/demo/models/Register.java
+++ src/main/java/com/example/demo/models/Register.java
@@ -1,14 +1,7 @@
 package com.example.demo.models;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.persistence.Transient;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
